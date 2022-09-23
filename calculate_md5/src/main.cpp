6fa10f04e647816feef986518287e48f2e585421
v138468 <v138468@gmail.com>
2022-09-23 13:19:35 +0800

    init

---
+++ calculate_md5/src/main.cpp
@@ -0,0 +1,35 @@
+#include <windows.h>
+#include "md5file.h"
+#include <iostream>
+#include <winbase.h>
+
+using namespace std;
+
+#define PATH "D:\\Desktop\\Monitor_TangWeiKang\\UpgradedMonitoringMachine_TangWeiKang\\RecordingSeparation\\code\\VS2019\\DownloadWget\\record.txt"
+
+int main(void)
+{
+
+    // while (1)
+    // {
+
+    //     //获取文件的MD5值
+    //     // twk测试
+    //     // std::cout << getFileMD5(PATH) << std::endl;
+
+    //     string FileMD5 = getFileMD5(PATH);
+    //     Sleep(1000);
+    //     if (FileMD5.compare(getFileMD5(PATH)) == 0)
+    //     {
+
+    //         cout << "文件没有被修改过" << endl;
+    //     }
+    //     else
+    //     {
+
+    //         cout << "文件被修改过l" << endl;
+    //     }
+    // }
+
+    return 0;
+}
